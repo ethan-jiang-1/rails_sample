d7966c7474e49031fd982ffe81e05b525b7a483e
jiangyizhen <ethan1.jiang@gmail.com>
2012-07-15 18:03:27 +0800

    have setup fundation of password - the encrypted_password and salt

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -104,13 +104,29 @@ describe User do
 
 
   describe "password encryption" do
-      before(:each) do
-        @user = User.create!(@attr)
+    before(:each) do
+      @user = User.create!(@attr)
+    end
+
+    it "should have an encrypted password attribute" do
+      @user.should respond_to(:encrypted_password)
+    end
+
+    it "should set the encrypted password" do
+      @user.encrypted_password.should_not be_blank
+    end
+
+    describe "has_password? method" do
+      it "should be true if the passwords match" do
+        @user.has_password?(@attr[:password]).should be_true
       end
 
-      it "should have an encrypted password attribute" do
-        @user.should respond_to(:encrypted_password)
+      it "should be false if the passwords don't match" do
+        @user.has_password?("invalid").should be_false
       end
+    end
+
+
   end
 
 end 

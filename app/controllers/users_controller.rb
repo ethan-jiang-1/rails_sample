e964d378f60acc2744a1c32c40f5c42f5706729d
jiangyizhen <ethan1.jiang@gmail.com>
2012-07-18 11:37:37 +0800

    before add faker.. still 2 repec test failed: title in new & index for non-signed-in user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,20 @@
 class UsersController < ApplicationController
+  before_filter :authenticate, :only => [:edit, :update]
+  before_filter :correct_user, :only => [:edit, :update]
+
   def show
     @user = User.find(params[:id])
     @title = @user.name
   end
 
+  def index
+    @title = "All users"
+    @users = User.all
+  end  
+
   def new
     @user = User.new
-    @title = "Sign up"
+    @title = @user.name
   end
 
   def create
@@ -20,6 +28,32 @@ class UsersController < ApplicationController
       @title = "Sign up"
       render 'new'
     end
+  end
+
+  def edit
+    @title = "Edit user"
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated."
+      redirect_to @user
+    else
+      @title = "Edit user"
+      render 'edit'
+    end
+  end
+
+  private
+
+  def authenticate
+    deny_access unless signed_in?
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_path) unless current_user?(@user)
   end
 
 end
